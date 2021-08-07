a31a1d77009632489d9a7562525e3a5aebbf27dc
Mark Weng <treesea20@gmail.com>
2021-08-07 17:41:50 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,91 @@
 <template>
-  <div id="basic-event">
-    <button @click="counter += 1">Add 1</button>
-    <p>The button above has been clicked {{ counter }} times.</p>
+  <dev>Num is {{ num }}</dev>
+  <br />
+  <input v-model="message" placeholder="edit me" />
+  <input v-model.lazy="message" />
+  <input v-model.trim="message" />
+  <p>Message is: {{ message }}</p>
+  <span>Multiline message is:</span>
+  <p style="white-space: pre-line;">{{ message }}</p>
+
+  <textarea v-model="message" placeholder="add multiple lines"></textarea>
+
+  <input type="checkbox" id="checkbox" v-model="checked" />
+  <label for="checkbox">{{ checked }}</label>
+
+  <div id="v-model-multiple-checkboxes">
+    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+    <label for="jack">Jack</label>
+    <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+    <label for="john">John</label>
+    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+    <label for="mike">Mike</label>
+    <br />
+    <span>Checked names: {{ checkedNames }}</span>
   </div>
-  <div id="event-with-method">
-    <!-- `greet` 是在下面定义的方法名 -->
-    <button @click="greet">Greet</button>
+
+  <div id="v-model-radiobutton">
+    <input type="radio" id="one" value="One" v-model="picked" />
+    <label for="one">One</label>
+    <br />
+    <input type="radio" id="two" value="Two" v-model="picked" />
+    <label for="two">Two</label>
+    <br />
+    <span>Picked: {{ picked }}</span>
   </div>
-  <div id="inline-handler">
-    <button @click="say('hi')">Say hi</button>
-    <button @click="say('what')">Say what</button>
+
+  <div id="v-model-select" class="demo">
+    <select v-model="selected">
+      <option disabled value>Please select one</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+    <span>Selected: {{ selected }}</span>
   </div>
-  <button @click="warn('Form cannot be submitted yet.', $event)">Submit</button>
+  <select v-model="selected" multiple>
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+  <br />
+  <span>Selected: {{ selected }}</span>
+
+  <div id="v-model-select-dynamic" class="demo">
+    <select v-model="selected2">
+      <option v-for="option in options" :value="option.value">{{ option.text }}</option>
+    </select>
+    <span>Selected: {{ selected2 }}</span>
+  </div>
+
+  <input v-model.number="age" type="number" />
+
+  <select v-model="selected">
+    <!-- 内联对象字面量 -->
+    <option :value="{ number: 123 }">123</option>
+  </select>
 </template>
 <script setup lang="ts" >
-import { reactive, ref, computed, watch } from 'vue'
-const counter = ref(0)
-const name = ref('Vue.js')
-function greet(event: PointerEvent) {
-  // `methods` 内部的 `this` 指向当前活动实例
-  alert('Hello ' + name.value + '!')
-  // `event` 是原生 DOM event
-  if (event) {
-    alert(event.target.tagName)
-    //console.log(event)
-  }
+import { reactive, ref, computed, watch, nextTick } from 'vue'
+const message = ref('')
+const checked = ref(true)
+const checkedNames = ref([])
+const picked = ref('')
+const selected = ref('')
+const selected2 = ref('')
+const options = reactive([
+  { text: 'One', value: 'A' },
+  { text: 'Two', value: 'B' },
+  { text: 'Three', value: 'C' }
+])
+const num = ref(0);
+console.log(new Date())
+for (let i = 0; i < 100000; i++) {
+  num.value = i
 }
-function say(message: string) {
-  alert(message)
-}
-function warn(message: string, event) {
-  // 现在可以访问到原生事件
-  if (event) {
-    event.preventDefault()
-  }
-  alert(message)
-}
+// await nextTick()
+// console.log(new Date())
+// console.log('Now DOM is updated')
 </script>
 
 <style>
