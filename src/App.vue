489e804d22a6e1426cf49d6beb30e0058dd127ba
Mark Weng <treesea20@gmail.com>
2021-08-24 10:38:26 +0800

    配置Web Components

--- src/App.vue
+++ src/App.vue
@@ -25,21 +25,18 @@
     {{ tab }}
   </button>
 
-<!-- Inactive components will be cached! -->
-<keep-alive>
-  <component :is="currentTabComponent">     </component>
-</keep-alive>
+
 </div>
 
 
-<div id="demo">
+<!-- <div id="demo">
   Push this button to do something you shouldn't be doing:<br>
 
   <div :class="{ shake: noActivated }">
     <button @click="noActivated = true">Click me</button>
     <span v-if="noActivated">Oh no!</span>
   </div>
-</div>
+</div> -->
 </template>
 <script setup lang="ts" >
 import { reactive, ref, computed, watch, defineCustomElement } from 'vue'
@@ -94,23 +91,23 @@ function addNewTodo() {
 // })
 // customElements.define('blog-gost2', Bloggost2)
 
-// const MyVueElement = defineCustomElement({
-//   // normal Vue component options here
-//   template: `<div class="demo-tab">Home component</div>`
-// })
-// customElements.define('tab-home', MyVueElement)
+const MyVueElement = defineCustomElement({
+  // normal Vue component options here
+  template: `<div class="demo-tab">Home component</div>`
+})
+customElements.define('tab-home', MyVueElement)
 
-// const MyVueElement2 = defineCustomElement({
-//   // normal Vue component options here
-//   template: `<div class="demo-tab">Posts component</div>`
-// })
-// customElements.define('tab-posts', MyVueElement2)
+const MyVueElement2 = defineCustomElement({
+  // normal Vue component options here
+  template: `<div class="demo-tab">Posts component</div>`
+})
+customElements.define('tab-posts', MyVueElement2)
 
-// const MyVueElement3 = defineCustomElement({
-//   // normal Vue component options here
-//   template: `<div class="demo-tab">Archive component</div>`
-// })
-// customElements.define('tab-archive', MyVueElement3)
+const MyVueElement3 = defineCustomElement({
+  // normal Vue component options here
+  template: `<div class="demo-tab">Archive component</div>`
+})
+customElements.define('tab-archive', MyVueElement3)
 
 const tabs = ['Home', 'Posts', 'Archive']
 const currentTab = ref('Home')
@@ -152,54 +149,54 @@ const currentTabComponent = computed(() => 'tab-' + currentTab.value.toLowerCase
 }
 </style>
 <style lang="scss">
-body {
-  margin: 30px;
-}
+// body {
+//   margin: 30px;
+// }
 
-button {
-  background: #d93419;
-  border-radius: 4px;
-  display: inline-block;
-  border: none;
-  padding: 0.75rem 1rem;
-  margin: 20px 10px 0 0;
-  text-decoration: none;
-  color: #ffffff;
-  font-family: sans-serif;
-  font-size: 1rem;
-  cursor: pointer;
-  text-align: center;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-}
+// button {
+//   background: #d93419;
+//   border-radius: 4px;
+//   display: inline-block;
+//   border: none;
+//   padding: 0.75rem 1rem;
+//   margin: 20px 10px 0 0;
+//   text-decoration: none;
+//   color: #ffffff;
+//   font-family: sans-serif;
+//   font-size: 1rem;
+//   cursor: pointer;
+//   text-align: center;
+//   -webkit-appearance: none;
+//   -moz-appearance: none;
+// }
 
-button:focus {
-  outline: 1px dashed #fff;
-  outline-offset: -3px;
-}
+// button:focus {
+//   outline: 1px dashed #fff;
+//   outline-offset: -3px;
+// }
 
-.shake {
-  animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
-  transform: translate3d(0, 0, 0);
-  backface-visibility: hidden;
-  perspective: 1000px;
-}
+// .shake {
+//   animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
+//   transform: translate3d(0, 0, 0);
+//   backface-visibility: hidden;
+//   perspective: 1000px;
+// }
 
-@keyframes shake {
-  10%, 90% {
-    transform: translate3d(-1px, 0, 0);
-  }
+// @keyframes shake {
+//   10%, 90% {
+//     transform: translate3d(-1px, 0, 0);
+//   }
   
-  20%, 80% {
-    transform: translate3d(2px, 0, 0);
-  }
+//   20%, 80% {
+//     transform: translate3d(2px, 0, 0);
+//   }
 
-  30%, 50%, 70% {
-    transform: translate3d(-4px, 0, 0);
-  }
+//   30%, 50%, 70% {
+//     transform: translate3d(-4px, 0, 0);
+//   }
 
-  40%, 60% {
-    transform: translate3d(4px, 0, 0);
-  }
-}
+//   40%, 60% {
+//     transform: translate3d(4px, 0, 0);
+//   }
+// }
 </style>
